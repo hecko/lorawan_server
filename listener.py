b751c3613be84eea07f34577e7929e044a23d764
Marcel Hecko <maco@blava.net>
2016-10-04 01:16:41 +0200

    more options

--- listener.py
+++ listener.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 
 import sys
-import base64
+import time
 import json
 import socket
 import struct
+import base64
+import argparse
 from pprint import pprint 
+from datetime import datetime
 from lora.crypto import loramac_decrypt
+
+parser = argparse.ArgumentParser(description='Semtech LoRaWAN packet forwarder listener')
+parser.add_argument('-v', '--verbose', action='store_true', help='Be verbose')
+args = parser.parse_args()
  
 UDP_IP = ""
 UDP_PORT = 1700 
@@ -20,6 +27,8 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(4000)
+    dt = datetime.now()
+    
     info['hex'] = " ".join("{:02x}".format(ord(c)) for c in data)
     info['protocol_version'] = ord(data[0])
     info['random_token'] = " ".join("{:02x}".format(ord(c)) for c in data[1:3]) 
@@ -27,17 +36,25 @@ while True:
     info['packet_type_human'] = packet_types[struct.unpack("B", data[3])[0]]
     info['gateway_mac_ident'] = " ".join("{:02x}".format(ord(c)) for c in data[4:12]) 
 
-    if info['packet_type_human'] == 'PKT_PUSH_DATA':
-        info['raw_payload'] = "".join("{:02x}".format(ord(c)) for c in data[12:]).decode('hex')
+    info['raw_payload'] = "".join("{:02x}".format(ord(c)) for c in data[12:]).decode('hex')
+
+    print(str(dt.isoformat("T")) + " received packet from forwarder (protocol version " + str(info['protocol_version']) + "). Packet type: " + info['packet_type_human'])
+
+    try:
         info['i'] = json.loads(info['raw_payload'])
+    except Exception as e:
+        pass
 
     if 'i' in info and info['packet_type_human'] == 'PKT_PUSH_DATA':
+
         if not 'rxpk' in info['i']:
             continue
-        '''The data payload is actually a PHYPayload from LoRaWAN spec.
+        '''The 'data' payload is actually a PHYPayload from LoRaWAN spec.
 
         ''' 
         d = info['i']['rxpk'][0]['data']
+
+        pprint(d)
 
         info['PHYPayload'] = []
         for c in base64.decodestring(d):
@@ -58,9 +75,11 @@ while True:
 
         payload = ''.join("{:02x}".format(c) for c in info['FRMPayload'])
 
-        print("Will try to decode the FRMPayload payload " + payload) 
+        if args.verbose:
+            print("Will try to decode the FRMPayload payload " + payload) 
         key = '820EB5127B0B98C8CC0B7EE43253E0D1'
 
         out = loramac_decrypt(payload, info['FCnt'], key, info['DevAddr'])
         info['decrypted_payload'] = out
-        pprint(info)
+        if args.verbose:
+            pprint(info)
